195895648b3052abff30cac8d1f5b5826a4fce08
pitash <pitashkumar03@gmail.com>
2024-07-10 00:00:26 +0600

    Part 2. Not use any DB. CRUD opearation manual data store in file system

--- index.js
+++ index.js
@@ -6,9 +6,38 @@
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object - module scaffolding
 const app = {};
+
+
+//testing file system
+
+// data.create('test', 'newFile', { name: 'bd', language: 'bangla' }, (err) => {
+//     if (err) {
+//         console.error('An error occurred:', err);
+//     } else {
+//         console.log('File created successfully');
+//     }
+// });
+
+// data.read('test', 'newFile', (err, result) => {
+//     console.log(err, result);
+// });
+
+// data.update('test', 'newFile', { name: 'Japan', language: 'Japanies' }, (err) => {
+//     if (err) {
+//         console.error('An error occurred:', err);
+//     } else {
+//         console.log('File update successfully');
+//     }
+// });
+
+// data.delete('test', 'newFile', (err) => {
+//     console.log(err);
+// });
 
 // configuration
 app.config = {
@@ -18,8 +47,8 @@ app.config = {
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`listening to port ${environment.port}`);
     });
 };
 
